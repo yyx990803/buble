82677c5543057102c13736746b427e1db5b5491c
Evan You <yyx990803@gmail.com>
2016-11-28 18:07:20 -0500

    cache _h

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -4,7 +4,8 @@ const names = 'Infinity,undefined,NaN,isFinite,isNaN,' +
   'parseFloat,parseInt,decodeURI,decodeURIComponent,encodeURI,encodeURIComponent,' +
   'Math,Number,Date,Array,Object,Boolean,String,RegExp,Map,Set,JSON,Intl,' +
   'require,' + // for webpack
-  'arguments' // parsed as identifier but is a special keyword...
+  'arguments,' + // parsed as identifier but is a special keyword...
+  '_h' // cached to save property access
 
 const hash = Object.create(null)
 names.split(',').forEach(name => {
