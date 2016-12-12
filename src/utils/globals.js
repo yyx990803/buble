a15f63765ba1c89b14c93164d3823f9a887855dc
Evan You <yyx990803@gmail.com>
2016-12-12 15:11:06 -0500

    fix new createElement adaption

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -5,7 +5,7 @@ const names = 'Infinity,undefined,NaN,isFinite,isNaN,' +
   'Math,Number,Date,Array,Object,Boolean,String,RegExp,Map,Set,JSON,Intl,' +
   'require,' + // for webpack
   'arguments,' + // parsed as identifier but is a special keyword...
-  '_h' // cached to save property access
+  '_h,_c' // cached to save property access (_c for ^2.1.5)
 
 const hash = Object.create(null)
 names.split(',').forEach(name => {
